0d3b5ca9e0a9f358b0aba75e551c00b1a8b35099
michaelhtran120 <michaelhtran120@gmail.com>
2021-09-01 13:13:47 -0700

    add all completed files

---
+++ README.md
@@ -0,0 +1,10 @@
+# First node.js project
+
+## Objective
+
+Set up a basic information page with
+
+- Home Page
+- About Page
+- Contact Page
+- 404 Page (if page can't be found)
---
+++ index.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(path.join(__dirname, "public", "home.html"), (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  } else if (req.url === "/about") {
+    fs.readFile(path.join(__dirname, "public", "about.html"), (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  } else if (req.url === "/contact-me") {
+    fs.readFile(
+      path.join(__dirname, "public", "contact-me.html"),
+      (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    );
+  } else {
+    fs.readFile(path.join(__dirname, "public", "404.html"), (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
